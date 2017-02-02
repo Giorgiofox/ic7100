23ef233681cf27fbe101d6a1deac729e05b1d8fa
Dennis <Dennis@DESKTOP2>
2017-02-02 10:50:50 -0800

    Add project files.

---
+++ Icom7100/Screen.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Screen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnButton1 = New System.Windows.Forms.Button()
+        Me.btnButton2 = New System.Windows.Forms.Button()
+        Me.btnButton3 = New System.Windows.Forms.Button()
+        Me.btnButton4 = New System.Windows.Forms.Button()
+        Me.btnButton5 = New System.Windows.Forms.Button()
+        Me.rtxFreq = New System.Windows.Forms.RichTextBox()
+        Me.rtxName = New System.Windows.Forms.RichTextBox()
+        Me.btnMode = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnButton1
+        '
+        Me.BtnButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnButton1.Location = New System.Drawing.Point(73, 503)
+        Me.BtnButton1.Name = "BtnButton1"
+        Me.BtnButton1.Size = New System.Drawing.Size(132, 58)
+        Me.BtnButton1.TabIndex = 0
+        Me.BtnButton1.Text = "Button1"
+        Me.BtnButton1.UseVisualStyleBackColor = True
+        '
+        'btnButton2
+        '
+        Me.btnButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnButton2.Location = New System.Drawing.Point(241, 503)
+        Me.btnButton2.Name = "btnButton2"
+        Me.btnButton2.Size = New System.Drawing.Size(132, 58)
+        Me.btnButton2.TabIndex = 1
+        Me.btnButton2.Text = "Button2"
+        Me.btnButton2.UseVisualStyleBackColor = True
+        '
+        'btnButton3
+        '
+        Me.btnButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnButton3.Location = New System.Drawing.Point(409, 503)
+        Me.btnButton3.Name = "btnButton3"
+        Me.btnButton3.Size = New System.Drawing.Size(132, 58)
+        Me.btnButton3.TabIndex = 2
+        Me.btnButton3.Text = "Button3"
+        Me.btnButton3.UseVisualStyleBackColor = True
+        '
+        'btnButton4
+        '
+        Me.btnButton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnButton4.Location = New System.Drawing.Point(577, 503)
+        Me.btnButton4.Name = "btnButton4"
+        Me.btnButton4.Size = New System.Drawing.Size(132, 58)
+        Me.btnButton4.TabIndex = 3
+        Me.btnButton4.Text = "Button4"
+        Me.btnButton4.UseVisualStyleBackColor = True
+        '
+        'btnButton5
+        '
+        Me.btnButton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnButton5.Location = New System.Drawing.Point(745, 503)
+        Me.btnButton5.Name = "btnButton5"
+        Me.btnButton5.Size = New System.Drawing.Size(132, 58)
+        Me.btnButton5.TabIndex = 4
+        Me.btnButton5.Text = "Button5"
+        Me.btnButton5.UseVisualStyleBackColor = True
+        '
+        'rtxFreq
+        '
+        Me.rtxFreq.Location = New System.Drawing.Point(219, 65)
+        Me.rtxFreq.Name = "rtxFreq"
+        Me.rtxFreq.Size = New System.Drawing.Size(523, 52)
+        Me.rtxFreq.TabIndex = 5
+        Me.rtxFreq.Text = ""
+        '
+        'rtxName
+        '
+        Me.rtxName.Location = New System.Drawing.Point(219, 123)
+        Me.rtxName.Name = "rtxName"
+        Me.rtxName.Size = New System.Drawing.Size(523, 52)
+        Me.rtxName.TabIndex = 6
+        Me.rtxName.Text = ""
+        '
+        'btnMode
+        '
+        Me.btnMode.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMode.Location = New System.Drawing.Point(219, 12)
+        Me.btnMode.Name = "btnMode"
+        Me.btnMode.Size = New System.Drawing.Size(75, 33)
+        Me.btnMode.TabIndex = 7
+        Me.btnMode.Text = "Mode"
+        Me.btnMode.UseVisualStyleBackColor = True
+        '
+        'Screen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(947, 582)
+        Me.Controls.Add(Me.btnMode)
+        Me.Controls.Add(Me.rtxName)
+        Me.Controls.Add(Me.rtxFreq)
+        Me.Controls.Add(Me.btnButton5)
+        Me.Controls.Add(Me.btnButton4)
+        Me.Controls.Add(Me.btnButton3)
+        Me.Controls.Add(Me.btnButton2)
+        Me.Controls.Add(Me.BtnButton1)
+        Me.Name = "Screen"
+        Me.Text = "Screen"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnButton1 As Button
+    Friend WithEvents btnButton2 As Button
+    Friend WithEvents btnButton3 As Button
+    Friend WithEvents btnButton4 As Button
+    Friend WithEvents btnButton5 As Button
+    Friend WithEvents rtxFreq As RichTextBox
+    Friend WithEvents rtxName As RichTextBox
+    Friend WithEvents btnMode As Button
+End Class
---
+++ Icom7100/Terminal.vb
@@ -0,0 +1,153 @@
+﻿Imports System
+Imports System.Threading
+Imports System.IO.Ports
+Imports System.ComponentModel
+Imports System.Text
+
+Public Class Terminal
+    Dim myPort As Array
+    Dim Count As Integer = 0
+    Dim prcCount As Integer = 0
+    Dim comBuffer As Byte()
+    Dim prcBuffer(2048) As Byte
+    Dim s0sub1(512) As Byte
+    Dim s0sub1Cnt As Integer
+    Private Delegate Sub UpdateFormDelegate(ByRef buff As Byte())
+    Dim UpdateformDelegate1 As UpdateFormDelegate
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        CmbPort.Items.AddRange(myPort)
+        CmbPort.Text = "COM6"
+        CmbBaud.Text = "38400"
+        BtnWrite.Enabled = False
+        Count = 0
+        prcCount = 0
+        Screen.Show()
+    End Sub
+
+    Private Sub BtnInit_Click(sender As Object, e As EventArgs) Handles BtnInit.Click
+        RtxOutput.Text = ""
+        SerialPort.PortName = CmbPort.Text
+        SerialPort.BaudRate = CmbBaud.Text
+        SerialPort.DataBits = 8
+        SerialPort.StopBits = StopBits.One
+        SerialPort.Parity = Parity.None
+        SerialPort.Handshake = Handshake.None
+        SerialPort.Open()
+
+        AddHandler SerialPort.DataReceived, AddressOf SerialPort_DataReceived
+
+        BtnInit.Enabled = False
+        BtnWrite.Enabled = True
+        BtnClose.Enabled = True
+    End Sub
+
+    Private Sub BtnWrite_Click(sender As Object, e As EventArgs) Handles BtnWrite.Click
+        Dim i, j As Integer
+        Dim value As Integer = 0
+        Dim numHex As Integer = 0
+        Dim byte1() As Byte = {0}
+
+        j = InStr(RtxInput.Text, "FD")
+        For i = 0 To j + 1
+            If numHex = 2 Then
+                SerialPort.Write(byte1, 0, 1)
+                byte1(0) = 0
+                numHex = 0
+                Continue For
+            End If
+            If (RtxInput.Text(i) >= "0" And RtxInput.Text(i) <= "9" And numHex < 2) Then
+                byte1(0) = byte1(0) * 16 + Asc(RtxInput.Text(i)) - Asc("0")
+                numHex += 1
+            ElseIf (RtxInput.Text(i) >= "A" And RtxInput.Text(i) <= "F" And numHex < 2) Then
+                byte1(0) = byte1(0) * 16 + Asc(RtxInput.Text(i)) - Asc("A") + 10
+                numHex += 1
+            End If
+        Next
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        SerialPort.Close()
+        BtnInit.Enabled = True
+        BtnWrite.Enabled = False
+        BtnClose.Enabled = False
+    End Sub
+
+    Private Sub SerialPort_DataReceived(ByVal sender As Object, ByVal e As SerialDataReceivedEventArgs)
+        'Handles SerialPort Data Received Events
+        UpdateformDelegate1 = New UpdateFormDelegate(AddressOf UpdateDisplay)
+        Count = SerialPort.BytesToRead  'We wouldn't be here if = 0
+        comBuffer = New Byte(Count - 1) {}  'Re dimension comBuffer
+        SerialPort.Read(comBuffer, 0, Count)
+        Me.Invoke(UpdateformDelegate1, comBuffer)  'Call UpdateDisplay() from this thread
+    End Sub
+
+    Private Sub UpdateDisplay(ByRef buffer As Byte())
+        Dim value As Short
+        Dim i As Integer
+
+        For i = 1 To Count
+            value = buffer(i - 1)
+            If Len(RtxOutput.Text) < 8192 Then
+                If value < 16 Then  'If < 16, add a zero
+                    RtxOutput.Text &= "0" & Format(value, "X")  'Put out hex digit
+                Else
+                    RtxOutput.Text &= Format(value, "X")    '>= 16, put out two hex digits
+                End If
+                If value >= 32 And value < 128 Then 'Put out ASCII character
+                    RtxOutput.Text &= Chr(value)
+                Else
+                    RtxOutput.Text &= " "   'If not ASCII, put out space
+                End If
+                RtxOutput.Text &= " "   'Space between bytes
+                If value = &HFD Then
+                    RtxOutput.Text &= vbCr  'End of line, put out Carriage Return
+                End If
+            End If
+            prcBuffer(prcCount) = CByte(value)
+            If prcBuffer(prcCount) = &HFD Then
+                ProcessLine()
+                prcCount = 0
+            Else
+                prcCount = prcCount + 1
+            End If
+        Next
+    End Sub
+
+    Private Sub ProcessLine()
+        Dim i As Integer
+
+        'For i = 1 To (prcCount - 1)
+        Select Case prcBuffer(1)
+            Case 0
+                ProcessScreen0(2)
+            Case Else
+                '    Thread.Sleep(5)
+        End Select
+        'Next
+    End Sub
+    Private Sub ProcessScreen0(ByVal pos As Integer)
+        Select Case (prcBuffer(pos))
+            Case &HA
+                Select Case (prcBuffer(pos + 1))
+                    Case &H1
+                        ProcessScreen0sub1(pos + 2)
+                End Select
+        End Select
+    End Sub
+    Private Sub ProcessScreen0sub1(ByVal pos As Integer)
+        Dim i As Integer
+
+        's0sub1Cnt = 0
+        'For i = pos To (prcCount - 1)
+        ' s0sub1(s0sub1Cnt) = prcBuffer(i)
+        's0sub1Cnt = s0sub1Cnt + 1
+        'Next
+        Screen.BtnButton1.Text = Encoding.ASCII.GetString(prcBuffer, 119, 5)
+        Screen.btnButton2.Text = Encoding.ASCII.GetString(prcBuffer, 124, 5)
+        Screen.btnButton3.Text = Encoding.ASCII.GetString(prcBuffer, 129, 5)
+        Screen.btnButton4.Text = Encoding.ASCII.GetString(prcBuffer, 134, 5)
+        Screen.btnButton5.Text = Encoding.ASCII.GetString(prcBuffer, 139, 5)
+    End Sub
+End Class
